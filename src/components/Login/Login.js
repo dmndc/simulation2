d429747f79056be091e1ef9e288f676654b63bb0
Leah Dombrovsky <leahliya59@gmail.com>
2017-11-03 15:56:01 -0500

    Leah version login screen

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import './Login.css';
+import axios from 'axios';
+import logo from '../../assets/auth_logo.png';
 
 // import { connect } from "react-redux";
 
@@ -14,18 +16,53 @@ class Login extends Component {
 
   }
 
+  loginUser = () => {
+    axios.post(`http//localhost:3000/api/auth/login`, {user: this.state.username, pass: this.state.password}).then(res => {
+      return res.data
+    })
+  }
+
+  registerUser =() => {
+    axios.post(`http//localhost:3000/api/auth/register`, {user: this.state.username, pass: this.state.password}).then(res => {
+      return res.data
+    })
+  }
+
+  handleUserName(event) {
+    this.setState({ username: event.currentTarget.value });
+  }
+
+  handlePassword(event) {
+    this.setState({ password: event.currentTarget.value });
+  }
+
   render() {
     return (
     <div>
-      <div>
+      {/* <div>
         <h1>HELLO WORLD!</h1>
-      </div>
+      </div> */}
        
       <div class="centralContainer">
 
-        <div class = "logo"></div>
-        <div class = "userName"></div>
-        <div class = "password"></div>
+        <img src={ logo } class = "logo" alt="logo"/>
+
+        
+        {/* <div class = "userName"> */}
+        <label>
+           Username:
+        </label>
+          <input type="text" username="name" />
+        {/* </div> */}
+
+        {/* <div class = "password"> */}
+        <label>
+           Password:
+        </label>
+          <input type="text" username="name" />
+          
+        {/* </div> */}
+        
         <div class ="buttonLogin"></div>
         <div class ="buttonRegister"></div>
 
