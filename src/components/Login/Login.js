b6830accbef7d4e15ace1829610f8e0a4c10d2df
Spencer Smith <WankFerret@Stevens-MacBook.local>
2017-11-03 14:47:37 -0500

    SpencerBranchUpdate 1

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './Login.css';
+import axios from 'axios'
 
 // import { connect } from "react-redux";
 
@@ -12,6 +13,26 @@ class Login extends Component {
       password: ''
     };
 
+  }
+
+  loginUser = () => {
+    axios.post(`http//localhost:3000/api/auth/login`, {user: this.state.username, pass: this.state.password}).then(res => {
+      return res.data
+    })
+  }
+
+  registerUser =() => {
+    axios.post(`http//localhost:3000/api/auth/register`, {user: this.state.username, pass: this.state.password}).then(res => {
+      return res.data
+    })
+  }
+
+  handleUserName(event) {
+    this.setState({ username: event.currentTarget.value });
+  }
+
+  handlePassword(event) {
+    this.setState({ password: event.currentTarget.value });
   }
 
   render() {
